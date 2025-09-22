73f797efd41f1cc8bde5a297510efa0da1f5eda7
Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
2025-09-22 17:19:51 +0200

    Feat: creating incident class, adding basic attiributes, adding DbSet to ApplicationDbContext

---
+++ src/AppForSEII2526.API/Models/Incident.cs
@@ -0,0 +1,14 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [Index(nameof(Title), IsUnique = true)]
+    public class Incident {
+        public int Id { get; set; }
+        [StringLength(50, ErrorMessage = "The title should be between 1 and 50 characters long", MinimumLength = 1)]
+        public string Title { get; set; }
+        [StringLength(256, ErrorMessage = "The description should be at most 256 characters long")]
+        public string Description { get; set; }
+        [StringLength(32, ErrorMessage = "The exercise name should be between 1 and 32 characters long")]
+        public string Exercise { get; set; }
+        public DateTime DateOfIdentification { get; set;}
+    }
+}
