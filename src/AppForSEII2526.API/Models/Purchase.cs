166afec51bdf1ae4e2d105a7214cc17ae182bb8f
luisv <Luis.Velaz@alu.uclm.es>
2025-09-22 13:08:45 +0200

    Created Purchase class, added attributes, first migration

---
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -0,0 +1,9 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Purchase
+    {
+        public int Id {get; set;}
+        public string City { get; set;} 
+        public string Country { get; set;}
+    }
+}
