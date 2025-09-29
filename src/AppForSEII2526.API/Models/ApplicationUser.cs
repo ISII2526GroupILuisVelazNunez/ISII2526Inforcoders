5be5d88fce56fe464df3b1c8b9f8cdbbf0bec13f
Juan Carlos Toboso Blázquez <99123090+jc-0001@users.noreply.github.com>
2025-09-29 12:47:56 +0200

    Task1 create class plan in models (#5)
    * Creadas clases Iniciales y modificada la de ApplicationUser en la carpeta Models
    
    * Modificadas clases Class y PlanItem de la carpeta Models para añadir un atributo que faltaba.
    
    * Making first migration
    
    * Modifying  classes Plan and PlanItem to make the class relationships

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,6 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+    public string Name { get; set; }
+    public string Surname { get; set; }
 }
