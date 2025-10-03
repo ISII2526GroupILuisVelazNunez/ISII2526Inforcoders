9e7764cdbe694ba11d22b103e7065d76feabeef6
Jorgelenny47 <101591320+Jorgelenny47@users.noreply.github.com>
2025-10-03 11:39:32 +0200

    Added IncidentItem class

---
+++ src/AppForSEII2526.API/Models/IncidentItem.cs
@@ -0,0 +1,22 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class IncidentItem
+    {
+        public int IncidentId { get; set; }
+        public int ItemId { get; set; }
+        public IncidentPriority IncidentPriority { get; set; }
+        public IncidentItem() { }
+        public IncidentItem(int incidentId, int itemId) 
+        {
+            IncidentId = incidentId;
+            ItemId = itemId;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is IncidentItem item &&
+                   IncidentId == item.IncidentId &&
+                   ItemId == item.ItemId;
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/Models/IncidentPriority.cs
@@ -0,0 +1,12 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class IncidentPriority
+    {
+        public enum Priority
+        {
+            High,
+            Medium,
+            Low
+        }
+    }
+}
