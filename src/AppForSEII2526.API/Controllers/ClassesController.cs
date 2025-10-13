76c4dd378d30cdd934548aa67e9e1e9a66b1f972
JUAN CARLOS TOBOSO BLAZQUEZ <juancarlos.toboso@alu.uclm.es>
2025-10-13 12:51:23 +0200

    Creating controller for Classes

---
+++ src/AppForSEII2526.API/Controllers/ClassesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassesController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ClassesController> _logger;
+
+        public ClassesController(ApplicationDbContext context, ILogger<ClassesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //[Route("[action]")]
+        //[ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        //[ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        //public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        //{
+        //    if (op2 == 0)
+        //    {
+        //        string error = "Division by zero is not allowed.";
+        //        _logger.LogError(DateTime.Now + "Error: " + error);
+        //        return BadRequest(error);
+        //    }
+        //    decimal result = op1 / op2;
+        //    return Ok(result);
+        //}
+    }
+}
