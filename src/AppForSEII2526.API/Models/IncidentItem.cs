a4d8253c567ad53d16bbc4ae369b49205e120e63
JorgeRamirezT <jorge.ramirez@alu.uclm.es>
2025-10-14 17:38:08 +0200

    Fix33 fixing classes associated with report incident use case (#34)
    * Renaming Class diagram file
    
    * Fixed Ids of ItemForExercise and added a dbset of it to ApplicationDbContext
    
    * Renaming ItemId to ItemForExerciseId, adding PrimaryKey on top of the class
    
    ---------
    
    Co-authored-by: Jorgelenny47 <101591320+Jorgelenny47@users.noreply.github.com>

--- src/AppForSEII2526.API/Models/IncidentItem.cs
+++ src/AppForSEII2526.API/Models/IncidentItem.cs
@@ -1,24 +1,25 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(IncidentId), nameof(ItemForExerciseId))]
     public class IncidentItem
     {
         public int IncidentId { get; set; }
         public Incident Incident { get; set; }
-        public int ItemId { get; set; }
+        public int ItemForExerciseId { get; set; }
         public ItemForExercise ItemForExercise { get; set; }
         public IncidentPriority IncidentPriority { get; set; }
         public IncidentItem() { }
         public IncidentItem(int incidentId, int itemId) 
         {
             IncidentId = incidentId;
-            ItemId = itemId;
+            ItemForExerciseId = itemId;
         }
 
         public override bool Equals(object? obj)
         {
             return obj is IncidentItem item &&
                    IncidentId == item.IncidentId &&
-                   ItemId == item.ItemId;
+                   ItemForExerciseId == item.ItemForExerciseId;
         }
     }
 }
