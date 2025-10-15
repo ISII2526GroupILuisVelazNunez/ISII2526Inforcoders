3b353f719b0e0733d514e34a0622d0c33db1f177
Jorgelenny47 <101591320+Jorgelenny47@users.noreply.github.com>
2025-10-15 11:21:14 +0200

    Added DTO and basic get method

---
+++ src/AppForSEII2526.API/DTOs/ItemForExerciseDTOs/ItemForReportingDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace AppForSEII2526.API.DTOs.ItemForExerciseDTOs
+{
+    public class ItemForReportingDTO
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public string Type { get; set; }
+
+        /*
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ItemForReportingDTO(int id, string name, string location, string description, string type)
+        {
+            Id = id;
+            Name = name;
+            Location = location;
+            Description = description;
+            Type = type;
+        }
+        */
+
+        public ItemForReportingDTO(int id, string location, string typeName)
+        {
+            Id = id;
+            Location = location;
+            Type = typeName;
+        }
+    }
+}
