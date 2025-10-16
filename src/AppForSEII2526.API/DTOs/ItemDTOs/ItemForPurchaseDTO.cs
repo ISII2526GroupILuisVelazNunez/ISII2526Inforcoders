37064ba1ba5b4e7fcb6b94deaf36e218ec5b41e3
luisv <Luis.Velaz@alu.uclm.es>
2025-10-16 22:30:24 +0200

    Gave ItemForPurchaseDTO the necessary attributes

--- src/AppForSEII2526.API/DTOs/ItemDTOs/ItemForPurchaseDTO.cs
+++ src/AppForSEII2526.API/DTOs/ItemDTOs/ItemForPurchaseDTO.cs
@@ -14,5 +14,14 @@
         [Required]
         public string Name { get; set; }
         public string Brand { get; set; }
+        [StringLength(70, ErrorMessage = "The description of the Item can be neither longer than 30 characters nor shorter than 1",
+        MinimumLength = 1)]
+        [Required]
+        public string Description { get; set; }
+        [Precision(10, 2)]
+        [Required]
+        public decimal PurchasePrice { get; set; }
+        [Required]
+        public int QuantityAvailableForPurchase { get; set; }
     }
 }
