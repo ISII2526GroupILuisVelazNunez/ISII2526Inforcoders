b03bf66fc4fe4738200aee95a10947eed8a2ae06
Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
2025-10-27 16:58:38 +0100

    Completed IncidentDetailDTO and IncidentItemDTO

--- src/AppForSEII2526.API/DTOs/IncidentDTOs/IncidentDetailDTO.cs
+++ src/AppForSEII2526.API/DTOs/IncidentDTOs/IncidentDetailDTO.cs
@@ -2,5 +2,19 @@
 {
     public class IncidentDetailDTO : IncidentForCreateDTO
     {
+        public IncidentDetailDTO(int id, IList<IncidentItemDTO> incidentItems, IncidentState incidentState, 
+            string title, DateTime dateOfIdentification, string exercise, string reporterName) 
+            : base(incidentItems, incidentState, title, dateOfIdentification, exercise, reporterName)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is IncidentDetailDTO dTO &&
+                   base.Equals(obj);
+        }
     }
 }
