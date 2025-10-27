8341be4aad81588f2051e069c0fe618ef18cbbe8
Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
2025-10-27 16:07:20 +0100

    Added incidents controller

---
+++ src/AppForSEII2526.API/Controllers/IncidentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncidentsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ItemsController> _logger;
+
+        public IncidentsController(ApplicationDbContext context, ILogger<ItemsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+    }
+}
