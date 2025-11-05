eee70b466d99ae3976967729640c6d3aed113d11
Jorgelenny47 <101591320+Jorgelenny47@users.noreply.github.com>
2025-11-05 12:01:04 +0100

    Added constructor to Aplication user, fixed constructor on IncidentItem

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,6 +4,13 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+    public ApplicationUser() { }
+    public ApplicationUser(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;
+    }
+
     public string Name { get; set; }
     public string Surname { get; set; }
     public IList<PaymentMethod> PaymentMethods { get; set; }
