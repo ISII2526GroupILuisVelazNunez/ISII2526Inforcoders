f9428a026c33d165ae04bbc4aa308d455939c3d7
Juan Carlos Toboso <JuanCarlos.Toboso@alu.uclm.es>
2025-11-06 13:12:31 +0100

    Created necessary DTOs and the get method for the plan details

---
+++ src/AppForSEII2526.API/DTOs/ClassDTOs/PlanForDetailDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace AppForSEII2526.API.DTOs.PlanDTOs
+{
+    public class PlanForDetailDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int Weeks { get; set; }
+        public string? HealthIssues { get; set; }
+
+        [Precision(9, 2)]
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UserFullName { get; set; } // "Nombre Apellido"
+
+        public IList<PlanItemForDetailDTO> PlanItems { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ClassDTOs/PlanItemForDetailDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace AppForSEII2526.API.DTOs.PlanDTOs
+{
+    public class PlanItemForDetailDTO
+    {
+        public int ClassId { get; set; }
+        public string Name { get; set; }
+        public IList<string> Type { get; set; } // ["Yoga", "Spinning"]
+
+        [Precision(9, 2)]
+        public decimal Price { get; set; } // price paid
+        public DateTime Date { get; set; }
+        public string? Goal { get; set; }
+    }
+}
