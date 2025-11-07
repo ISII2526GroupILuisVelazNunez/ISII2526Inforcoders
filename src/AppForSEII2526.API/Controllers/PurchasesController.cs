8a038b478c4f7a4c37c5663489c79e1fdc895661
luisv <Luis.Velaz@alu.uclm.es>
2025-11-07 11:56:38 +0100

    Created ItemForPurchaseDetailDTO (only shows specific attributes), changed PurchaseDetailDTO, adapted method in PurchasesController. Failed to test with real Purchase data because of PaymentMethod asking for Users. Will work on a new issue to automatically add Users to database

---
+++ src/AppForSEII2526.API/Controllers/PurchasesController.cs
@@ -0,0 +1,66 @@
+﻿using AppForSEII2526.API.DTOs.ItemDTOs;
+using AppForSEII2526.API.DTOs.PurchaseDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchasesController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ItemsController> _logger;
+
+        public PurchasesController(ApplicationDbContext context, ILogger<ItemsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(PurchaseDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetPurchase(int id)
+        {
+            if (_context.Purchases == null)
+            {
+                _logger.LogError("Error: Purchases table does not exist.");
+                return NotFound();
+            }
+
+            var purchase = await _context.Purchases
+                .Where(p => p.Id == id)
+                .Include(p => p.PurchaseItems)          // join PurchaseItems
+                    .ThenInclude(pi => pi.Item)         // then join Items
+                .Select(p => new PurchaseDetailDTO(
+                    p.Id,
+                    p.PaymentMethod,
+                    p.Street,
+                    p.City,
+                    p.Country,
+                    p.Description,         
+                    p.PurchaseItems
+                        .Select(pi => new ItemForPurchaseDetailDTO(
+                            pi.Item.Name,
+                            pi.Item.Brand.Name,
+                            pi.Amount_bought,
+                            pi.Price
+                        ))
+                        .ToList(),
+                    p.Total_price
+                ))
+                .FirstOrDefaultAsync();
+
+            if (purchase == null)
+            {
+                _logger.LogError($"Error: Purchase with id {id} does not exist.");
+                return NotFound();
+            }
+
+            return Ok(purchase);
+        }
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseDetailDTO.cs
@@ -0,0 +1,36 @@
+﻿using AppForSEII2526.API.DTOs.ItemDTOs;
+
+namespace AppForSEII2526.API.DTOs.PurchaseDTOs
+{
+    public class PurchaseDetailDTO
+    {
+        public PurchaseDetailDTO(int id, PaymentMethod paymentMethod, string street, string city, string country, string? description, List<ItemForPurchaseDetailDTO> purchaseItems, decimal total_price)
+        {
+            Id = id;
+            PaymentMethod = paymentMethod;
+            Street = street;
+            City = city;
+            Country = country;
+            Description = description;
+            PurchaseItems = purchaseItems;
+            Total_price = total_price;
+        }
+        public int Id { get; set; }
+        [Required]
+        public string DeliveryAddress => $"{Street}, {City}, {Country}";
+        [Precision(10, 2)]
+        public decimal Total_price { get; set; }
+        [StringLength(200, ErrorMessage = "Description must be shorter than 200 characters.")]
+        public string? Description { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, specify the street.")]
+        public string Street { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, specify the city.")]
+        public string City { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, specify the country.")]
+        public string Country { get; set; }
+        [Required]
+        List<ItemForPurchaseDetailDTO> PurchaseItems { get; set; }
+    }
+}
+
