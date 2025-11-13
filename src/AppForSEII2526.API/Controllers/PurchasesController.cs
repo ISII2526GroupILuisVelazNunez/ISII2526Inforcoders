854f229291b88058736358d68f0586b78f628226
Luis Vélaz Núñez <129756115+luisVNplayer@users.noreply.github.com>
2025-11-13 19:36:11 +0100

    Task 44 develop post method por creating a purchase (#60)
    * Added constructors to Purchase and PurchaseItem, created DTO and started Controller for Purchases to create them
    
    * Created the DTO to show the details of a purchase, switching to properly developing GET method for details for now for later merge
    
    * Changed incorrect name to PurchaseDetailDTO, class will be created in task 50 too
    
    * Changed DTOs, created ItemForPurchaseSelectionDTO, changed PurchasesController accordingly, created new constructors in classes, seems to work, to test
    
    * Seeded data to automatically generate some items
    
    * SeedData with some items
    
    * Added empty list to Purchase constructor to fix a NullPointerException error, fixed the calculation of total price of the purchase, pull request

--- src/AppForSEII2526.API/Controllers/PurchasesController.cs
+++ src/AppForSEII2526.API/Controllers/PurchasesController.cs
@@ -63,9 +63,125 @@ namespace AppForSEII2526.API.Controllers
                 _logger.LogError($"Error: Purchase with id {id} does not exist.");
                 return NotFound();
             }
+            // Validation and save
+            if (ModelState.ErrorCount > 0)
+                return BadRequest(new ValidationProblemDetails(ModelState));
 
             return Ok(purchase);
         }
+
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.Conflict)]
+        [ProducesResponseType(typeof(PurchaseDetailDTO), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult> CreatePurchase(PurchaseForCreateDTO purchaseForCreate)
+        {
+            // --- Validate required fields ---
+            if (string.IsNullOrWhiteSpace(purchaseForCreate.Street))
+                ModelState.AddModelError("Street", "Error! You must specify a street.");
+
+            if (string.IsNullOrWhiteSpace(purchaseForCreate.City))
+                ModelState.AddModelError("City", "Error! You must specify a city.");
+
+            if (string.IsNullOrWhiteSpace(purchaseForCreate.Country))
+                ModelState.AddModelError("Country", "Error! You must specify a country.");
+
+            if (purchaseForCreate.PurchaseItems == null || !purchaseForCreate.PurchaseItems.Any())
+                ModelState.AddModelError("PurchaseItems", "Error! You must include at least one item to be purchased.");
+
+            if (ModelState.ErrorCount > 0)
+                return BadRequest(new ValidationProblemDetails(ModelState));
+
+            // --- Get user's payment method ---
+            var paymentMethod = await _context.PaymentMethods
+                .FirstOrDefaultAsync(pm => pm.Id == purchaseForCreate.PaymentMethodId);
+            if (paymentMethod == null)
+            {
+                ModelState.AddModelError("PaymentMethodId", "Error! Selected payment method does not exist.");
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+
+            // --- Fetch items from DB ---
+            var itemIds = purchaseForCreate.PurchaseItems.Select(pi => pi.Id).ToList();
+            var items = await _context.Items
+                .Include(i => i.Brand)
+                .Where(i => itemIds.Contains(i.Id))
+                .ToListAsync();
+
+            // --- Create the purchase entity ---
+            var purchase = new Purchase(
+                purchaseForCreate.City,
+                purchaseForCreate.Country,
+                DateTime.Now,
+                purchaseForCreate.Description,
+                purchaseForCreate.Street
+            );
+
+            purchase.PaymentMethod = paymentMethod;
+
+            //process all of the selected items
+            foreach (var selectedItem in purchaseForCreate.PurchaseItems)
+            {
+                var dbItem = items.FirstOrDefault(i => i.Id == selectedItem.Id);
+                if (dbItem == null)
+                {
+                    ModelState.AddModelError("PurchaseItems", $"Item '{selectedItem.Name}' does not exist.");
+                    continue;
+                }
+
+                if (selectedItem.QuantityToBuy > dbItem.QuantityAvailableForPurchase)
+                {
+                    ModelState.AddModelError("PurchaseItems",
+                        $"Requested {selectedItem.QuantityToBuy} of '{dbItem.Name}', but only {dbItem.QuantityAvailableForPurchase} available.");
+                    continue;
+                }
+
+                //create purchaseItem
+                var purchaseItem = new PurchaseItem(
+                    dbItem.Id,
+                    selectedItem.QuantityAvailableForPurchase,
+                    selectedItem.QuantityToBuy,
+                    dbItem.PurchasePrice,
+                    purchase.Id);
+
+                purchase.PurchaseItems.Add(purchaseItem);
+
+                // Reduce stock
+                dbItem.QuantityAvailableForPurchase -= selectedItem.QuantityToBuy;
+            }
+
+            if (ModelState.ErrorCount > 0)
+                return BadRequest(new ValidationProblemDetails(ModelState));
+
+            //total_price calculation
+            purchase.Total_price = purchase.PurchaseItems.Sum(pi => pi.Price * pi.Amount_bought);
+
+            //save
+            _context.Purchases.Add(purchase);
+            await _context.SaveChangesAsync();
+
+            //the return DTO
+            var purchaseDetail = new PurchaseDetailDTO(
+                purchase.Id,
+                new PaymentMethodForPurchaseDetailDTO(paymentMethod.Id, paymentMethod.GetType().Name),
+                purchase.Street,
+                purchase.City,
+                purchase.Country,
+                purchase.Description,
+                purchase.PurchaseItems.Select(pi => new ItemForPurchaseDetailDTO(
+                    pi.Item.Name,
+                    pi.Item.Brand.Name,
+                    pi.Amount_bought,
+                    pi.Price
+                )).ToList(),
+                purchase.Total_price
+            );
+
+            return CreatedAtAction("GetPurchase", new { id = purchase.Id }, purchaseDetail);
+        }
+
+
 
 
     }
--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -4,6 +4,43 @@ namespace AppForSEII2526.API.Models
 {
     public class Purchase
     {
+        public Purchase(int id, string city, string country, DateTime date, string description, string street, IList<PurchaseItem> purchaseItems, decimal total_price, PaymentMethod paymentMethod)
+        {
+            Id = id;
+            City = city;
+            Country = country;
+            Date = date;
+            Description = description;
+            Street = street;
+            PurchaseItems = purchaseItems;
+            Total_price = total_price;
+            PaymentMethod = paymentMethod;
+        }
+        public Purchase(string city, string country, DateTime date, string? description, string street, List<PurchaseItem> purchaseItems, decimal total_price, PaymentMethod paymentMethod)
+        {
+            City = city;
+            Country = country;
+            Date = date;
+            Description = description;
+            Street = street;
+            PurchaseItems = purchaseItems;
+            Total_price = total_price;
+            PaymentMethod = paymentMethod;
+        }
+
+        //fix error
+        public Purchase(string city, string country, DateTime date, string? description, string street)
+        {
+            City = city;
+            Country = country;
+            Date = date;
+            Description = description;
+            Street = street;
+            PurchaseItems = new List<PurchaseItem>();
+        }
+
+        public Purchase() { } // parameterless
+
         public int Id {get; set;}
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please, specify the city.")]
         public string City { get; set;}
