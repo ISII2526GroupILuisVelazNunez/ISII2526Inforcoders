8949b43fc2fe080831a077d004aed5b9e56a96bf
Luis Vélaz Núñez <129756115+luisVNplayer@users.noreply.github.com>
2025-11-13 17:20:20 +0100

    Task 50 developing get method to see the details of a purchase (#58)
    * Created ItemForPurchaseDetailDTO (only shows specific attributes), changed PurchaseDetailDTO, adapted method in PurchasesController. Failed to test with real Purchase data because of PaymentMethod asking for Users. Will work on a new issue to automatically add Users to database
    
    * Merged with development, adjusted DTOs to show proper information, deleted migrations, prepared for Pull Request

---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseDetailDTO.cs
@@ -0,0 +1,39 @@
+﻿using AppForSEII2526.API.DTOs.ItemDTOs;
+
+namespace AppForSEII2526.API.DTOs.PurchaseDTOs
+{
+    public class PurchaseDetailDTO
+    {
+        public PurchaseDetailDTO(int id, PaymentMethodForPurchaseDetailDTO paymentMethod, string street, string city, string country, string? description, List<ItemForPurchaseDetailDTO> purchaseItems, decimal total_price)
+        {
+            Id = id;
+            PaymentMethod = paymentMethod;
+            Street = street;
+            City = city;
+            Country = country;
+            Description = description;
+            PurchaseItems = purchaseItems;
+            Total_price = total_price;
+        }
+        public int Id { get; set; }
+        [Required]
+        public string DeliveryAddress => $"{Street}, {City}, {Country}";
+        [Precision(10, 2)]
+        public decimal Total_price { get; set; }
+        [StringLength(200, ErrorMessage = "Description must be shorter than 200 characters.")]
+        public string? Description { get; set; }
+        public PaymentMethodForPurchaseDetailDTO PaymentMethod { get; set; }
+        [System.Text.Json.Serialization.JsonIgnore]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, specify the street.")]
+        public string Street { get; set; }
+        [System.Text.Json.Serialization.JsonIgnore]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, specify the city.")]
+        public string City { get; set; }
+        [System.Text.Json.Serialization.JsonIgnore]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, specify the country.")]
+        public string Country { get; set; }
+        [Required]
+        public List<ItemForPurchaseDetailDTO> PurchaseItems { get; set; }
+    }
+}
+
