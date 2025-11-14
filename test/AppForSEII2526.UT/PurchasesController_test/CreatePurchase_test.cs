8287591b3f89d6773d4def02e566b5deb7f67025
luisv <Luis.Velaz@alu.uclm.es>
2025-11-14 11:37:25 +0100

    Merged with development, created CreatePurchase_test, to fill

---
+++ test/AppForSEII2526.UT/PurchasesController_test/CreatePurchase_test.cs
@@ -0,0 +1,19 @@
+﻿using AppForSEII2526.API.Controllers;
+using AppForSEII2526.API.DTOs.ItemDTOs;
+using AppForSEII2526.API.DTOs.PurchaseDTOs;
+using AppForSEII2526.API.Models;
+using AppForSEII2526.UT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppForSEII2526.UT.PurchasesController_test
+{
+    public class CreatePurchase_test : AppForSEII25264SqliteUT 
+    {
+        
+    }
+}
