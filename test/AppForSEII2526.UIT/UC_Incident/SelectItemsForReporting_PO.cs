ef07939fec7b4ff1f1f66b6b096c77655cea165b
Jorgelenny47 <101591320+Jorgelenny47@users.noreply.github.com>
2025-12-03 10:57:43 +0100

    Added Id to form control fields and added method to simulate searching for items by name

--- test/AppForSEII2526.UIT/UC_Incident/SelectItemsForReporting_PO.cs
+++ test/AppForSEII2526.UIT/UC_Incident/SelectItemsForReporting_PO.cs
@@ -8,6 +8,17 @@ namespace AppForSEII2526.UIT.UC_Incident
 {
     public class SelectItemsForReporting_PO : PageObject
     {
+        By itemName = By.Id("itemName");
+        By itemLocation = By.Id("itemLocation");
+        By searchButton = By.Id("searchItems");
+
+        public void SearchItems(string name)
+        {
+            WaitForBeingClickable(itemName);
+            _driver.FindElement(itemName).SendKeys(name);
+            _driver.FindElement(searchButton).Click();
+        }
+
         public  SelectItemsForReporting_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
         }
