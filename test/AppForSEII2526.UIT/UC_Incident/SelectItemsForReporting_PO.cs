db0d11057bd3d8992934acd03ae1f889749c3da0
Jorgelenny47 <101591320+Jorgelenny47@users.noreply.github.com>
2025-12-03 10:41:17 +0100

    Created PageObject to interact with select items for reporting

---
+++ test/AppForSEII2526.UIT/UC_Incident/SelectItemsForReporting_PO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Incident
+{
+    public class SelectItemsForReporting_PO : PageObject
+    {
+        public  SelectItemsForReporting_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+    }
+}
