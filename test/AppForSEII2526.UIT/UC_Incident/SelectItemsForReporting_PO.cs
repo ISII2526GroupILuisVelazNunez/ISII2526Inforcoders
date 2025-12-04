3ba22b3ddd0729c79394e2e425f521c60e8d2350
Jorgelenny47 <101591320+Jorgelenny47@users.noreply.github.com>
2025-12-04 08:31:00 +0100

    Added table base and Id to the menu tab

--- test/AppForSEII2526.UIT/UC_Incident/SelectItemsForReporting_PO.cs
+++ test/AppForSEII2526.UIT/UC_Incident/SelectItemsForReporting_PO.cs
@@ -11,6 +11,7 @@ namespace AppForSEII2526.UIT.UC_Incident
         By itemName = By.Id("itemName");
         By itemLocation = By.Id("itemLocation");
         By searchButton = By.Id("searchItems");
+        By itemsTable = By.Id("itemsTable");
 
         public void SearchItems(string name, string location)
         {
@@ -22,6 +23,11 @@ namespace AppForSEII2526.UIT.UC_Incident
 
             WaitForBeingClickable(searchButton);
             _driver.FindElement(searchButton).Click();
+        }
+
+        public bool CheckListOfItems(List<string[]> expectedItems)
+        {
+            return CheckBodyTable(expectedItems, itemsTable);
         }
 
         public  SelectItemsForReporting_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
