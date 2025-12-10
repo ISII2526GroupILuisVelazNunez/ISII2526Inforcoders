b84e2867502fc586d4c37220e29bb02324b6596f
Juan Carlos Toboso Blázquez <99123090+jc-0001@users.noreply.github.com>
2025-12-10 18:27:55 +0100

    Task77 develop UI for selecting classes for a plan (#79)
    * Creating PlanStateContainer, migrations and the SelectClassesForPlan razor component
    
    * URL bugfix
    
    * web settings bug fix
    
    * tidying code

--- src/AppForSEII2526.API/Migrations/20251201113028_CreateIdentitySchema.Designer.cs
+++ src/AppForSEII2526.API/Migrations/20251201113028_CreateIdentitySchema.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppForSEII2526.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251117102943_CreateIdentitySchema")]
+    [Migration("20251201113028_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
         /// <inheritdoc />
