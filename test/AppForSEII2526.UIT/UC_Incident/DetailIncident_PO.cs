e46ad537493260dcc362b6bdae893cb9e1433656
Jorgelenny47 <101591320+Jorgelenny47@users.noreply.github.com>
2025-12-12 18:46:48 +0100

    Added methods to check details of report and incident items in PO

--- test/AppForSEII2526.UIT/UC_Incident/DetailIncident_PO.cs
+++ test/AppForSEII2526.UIT/UC_Incident/DetailIncident_PO.cs
@@ -8,6 +8,26 @@ namespace AppForSEII2526.UIT.UC_Incident
 {
     public class DetailIncident_PO : PageObject
     {
+        public bool CheckItemsTable(List<string[]> expectedItems)
+        {
+            return CheckBodyTable(expectedItems, By.Id("reportedItems"));
+        }
+
+        public bool CheckIncidentDetails(string title, DateTime dateOfIdentification, 
+            string exercise, string reporterName)
+        {
+            System.Threading.Thread.Sleep(1000);
+            bool result = true;
+            result &= _driver.FindElement(By.Id("Title")).Text.Contains(title);
+            result &= _driver.FindElement(By.Id("Exercise")).Text.Contains(exercise);
+            result &= _driver.FindElement(By.Id("ReporterName")).Text.Contains(reporterName);
+
+            var actualDateOfIdent = DateTime.Parse(_driver.FindElement(By.Id("DateOfIdent")).Text);
+            result &= ((actualDateOfIdent - dateOfIdentification) < new TimeSpan(0, 1, 0));
+
+            return result;
+        }
+
         public DetailIncident_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
         }
