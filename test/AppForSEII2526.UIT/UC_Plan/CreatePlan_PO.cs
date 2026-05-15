88f09efe593354f66c4396f537f44c627c54fd9e
JUAN CARLOS TOBOSO BLAZQUEZ <juancarlos.toboso@alu.uclm.es>
2026-05-15 19:47:17 +0200

    Modified previous Page Objects to have the ones needed

---
+++ test/AppForSEII2526.UIT/UC_Plan/CreatePlan_PO.cs
@@ -0,0 +1,39 @@
+﻿namespace AppForSEII2526.UIT.UC_Plan
+{
+    public class CreatePlan_PO : PageObject
+    {
+
+        private By _planNameBy = By.Id("planName");
+        private IWebElement _planName() => _driver.FindElement(_planNameBy);
+        private IWebElement _weeks() => _driver.FindElement(By.Id("planWeeks"));
+        private IWebElement _paymentMethod() => _driver.FindElement(By.Id("paymentMethod"));
+        private IWebElement _submitButton() => _driver.FindElement(By.Id("submitPlan"));
+
+        public CreatePlan_PO(IWebDriver driver, ITestOutputHelper output)
+            : base(driver, output)
+        {
+        }
+
+        public void FillInPlanInfo(string name, int weeks, string paymentMethodText)
+        {
+            WaitForBeingVisible(_planNameBy);
+            _planName().Clear();
+            _planName().SendKeys(name);
+            _weeks().Clear();
+            _weeks().SendKeys(weeks.ToString());
+
+            SelectElement selectElement = new SelectElement(_paymentMethod());
+            selectElement.SelectByText(paymentMethodText);
+        }
+
+        public void PressSavePlan()
+        {
+            _submitButton().Click();
+        }
+
+        public bool CheckValidationError(string expectedError)
+        {
+            return _driver.PageSource.Contains(expectedError);
+        }
+    }
+}
